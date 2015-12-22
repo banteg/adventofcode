849ae6688e7e294601343935c8656bea51f3749a
Artem Kharlamov <banteeg@gmail.com>
2015-12-23 04:05:57 +0600

    solve Day 3: Part Two

--- day_03.py
+++ day_03.py
@@ -12,6 +12,20 @@ For example:
 > delivers presents to 2 houses: one at the starting location, and one to the east.
 ^>v< delivers presents to 4 houses in a square, including twice to the house at his starting/ending location.
 ^v^v^v^v^v delivers a bunch of presents to some very lucky children at only 2 houses.
+
+--- Part Two ---
+
+The next year, to speed up the process, Santa creates a robot version of himself, Robo-Santa, to deliver presents with him.
+
+Santa and Robo-Santa start at the same location (delivering two presents to the same starting house), then take turns moving based on instructions from the elf, who is eggnoggedly reading from the same script as the previous year.
+
+This year, how many houses receive at least one present?
+
+For example:
+
+^v delivers presents to 3 houses, because Santa goes north, and then Robo-Santa goes south.
+^>v< now delivers presents to 3 houses, and Santa and Robo-Santa end up back where they started.
+^v^v^v^v^v now delivers presents to 11 houses, with Santa going one direction and Robo-Santa going the other.
 '''
 
 
@@ -28,18 +42,43 @@ def deliver_presents(instructions):
     return len(visited)
 
 
+def robo_santa(instructions):
+    commands = {'^': (0, -1), 'v': (0, 1), '>': (1, 0), '<': (-1, 0)}
+    visited = {(0, 0): True}
+    sx, sy = 0, 0
+    rx, ry = 0, 0
+    for c, i in enumerate(instructions, 1):
+        dx, dy = commands[i]
+        if c % 2:
+            sx += dx
+            sy += dy
+            visited[(sx, sy)] = True
+        else:
+            rx += dx
+            ry += dy
+            visited[(rx, ry)] = True
+
+    return len(visited)
+
+
 def read_instructions(file):
     with open(file) as f:
         data = f.read().strip()
+
     delivery = deliver_presents(data)
     print(delivery)
-    return delivery
+
+    robo_delivery = robo_santa(data)
+    print(robo_delivery)
 
 
 def tests():
     assert deliver_presents('>') == 2
     assert deliver_presents('^>v<') == 4
     assert deliver_presents('^v^v^v^v^v') == 2
+    assert robo_santa('^v') == 3
+    assert robo_santa('^>v<') == 3
+    assert robo_santa('^v^v^v^v^v') == 11
 
 
 tests()
