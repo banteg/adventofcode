1a743b99d01a4d5e745cc62855fc2758a093a63e
Artem Kharlamov <banteeg@gmail.com>
2015-12-24 01:54:00 +0600

    solve Day 17: Part Two

--- day_17.py
+++ day_17.py
@@ -10,6 +10,14 @@ For example, suppose you have containers of size 20, 15, 10, 5, and 5 liters. If
 20 and 5 (the second 5)
 15, 5, and 5
 Filling all containers entirely, how many different combinations of containers can exactly fit all 150 liters of eggnog?
+
+--- Part Two ---
+
+While playing with all the containers in the kitchen, another load of eggnog arrives! The shipping and receiving department is requesting as many containers as you can spare.
+
+Find the minimum number of containers that can exactly fit all 150 liters of eggnog. How many different ways can you fill that number of containers and still hold exactly 150 litres?
+
+In the example above, the minimum number of containers was two. There were three ways to use that many containers, and so the answer there would be 3.
 '''
 
 
@@ -32,6 +40,10 @@ def read_instructions(file):
 
     storage = list(store(instructions, 150))
     print(len(storage))
+
+    least = min([len(x) for x in storage])
+    compact = sum([1 for x in storage if len(x) == least])
+    print(compact)
 
 
 read_instructions('inputs/day_17.txt')
