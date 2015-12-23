cc67979c31b67c91e53e1795c11112ab43cac2af
Artem Kharlamov <banteeg@gmail.com>
2015-12-23 16:13:41 +0600

    solve Day 5: Part Two

--- day_05.py
+++ day_05.py
@@ -16,6 +16,22 @@ jchzalrnumimnmhp is naughty because it has no double letter.
 haegwjzuvuyypxyu is naughty because it contains the string xy.
 dvszwmarrgswjxmb is naughty because it contains only one vowel.
 How many strings are nice?
+
+--- Part Two ---
+
+Realizing the error of his ways, Santa has switched to a better model of determining whether a string is naughty or nice. None of the old rules apply, as they are all clearly ridiculous.
+
+Now, a nice string is one with all of the following properties:
+
+It contains a pair of any two letters that appears at least twice in the string without overlapping, like xyxy (xy) or aabcdefgaa (aa), but not like aaa (aa, but it overlaps).
+It contains at least one letter which repeats with exactly one letter between them, like xyx, abcdefeghi (efe), or even aaa.
+For example:
+
+qjhvhtzxzqqjkmpb is nice because is has a pair that appears twice (qj) and a letter that repeats with exactly one letter between them (zxz).
+xxyxx is nice because it has a pair that appears twice and a letter that repeats with one between, even though the letters used by each rule overlap.
+uurcxstgmygtbstg is naughty because it has a pair (tg) but no repeat with a single letter between them.
+ieodomkazucvgmuy is naughty because it has a repeating letter with one between (odo), but no pair that appears twice.
+How many strings are nice under these new rules?
 '''
 
 import re
@@ -28,12 +44,23 @@ def is_nice(s):
     return bool(len(vovels) >= 3 and twice and not bad)
 
 
+def is_nice_two(s):
+    pair = re.findall(r'(..).*\1', s)
+    repeat = re.findall(r'(.).\1', s)
+    return bool(pair and repeat)
+
+
 def test():
     assert is_nice('ugknbfddgicrmopn') == True
     assert is_nice('aaa') == True
     assert is_nice('jchzalrnumimnmhp') == False
     assert is_nice('haegwjzuvuyypxyu') == False
     assert is_nice('dvszwmarrgswjxmb') == False
+
+    assert is_nice_two('qjhvhtzxzqqjkmpb') == True
+    assert is_nice_two('xxyxx') == True
+    assert is_nice_two('uurcxstgmygtbstg') == False
+    assert is_nice_two('ieodomkazucvgmuy') == False
 
 
 def find_out_nice(file):
@@ -42,6 +69,9 @@ def find_out_nice(file):
 
     result = sum(map(is_nice, inputs))
     print(result)
+
+    result_two = sum(map(is_nice_two, inputs))
+    print(result_two)
 
 
 test()
