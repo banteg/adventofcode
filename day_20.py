be395c23d41f5be211f8dfa34e9297e5dd1f04fd
Artem Kharlamov <banteeg@gmail.com>
2015-12-24 05:19:58 +0600

    solve Day 20: Part Two

--- day_20.py
+++ day_20.py
@@ -2,16 +2,22 @@ from click import progressbar
 from numpy import zeros, arange
 
 limit = 36000000
-hood = zeros(limit // 10)
 
 
-def deliver_presents():
+def deliver_presents(limited=False):
+    hood = zeros(limit // 10, dtype=int)
     with progressbar(arange(1, limit // 10)) as bar:
         for elf in bar:
-            hood[elf::elf] += elf * 10
+            if limited:
+                hood[elf:elf * 50 + 1:elf] += elf * 11
+            else:
+                hood[elf::elf] += elf * 10
+
+    cond = ((k, v) for k, v in enumerate(hood) if v >= limit)
+    result = min(cond)
+    print(result)
+    return result
 
 
 deliver_presents()
-cond = ((k, v) for k, v in enumerate(hood) if v >= limit)
-result = min(cond)
-print(result)
+deliver_presents(limited=True)
